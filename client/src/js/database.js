a47ac9d8944fca59845418857797ae9967c779b1
rob <collinsduo1@gmail.com>
2023-10-03 13:55:17 -0500

    add dist

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  const jateDb = await openDB('jate', 1); 
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1,value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value); 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.get(1);
+
+  // Get confirmation of the request.
+  const result = await request;
+  result
+    ? console.log("🚀 - data retrieved from the database", result.value)
+    : console.log("🚀 - data not found in the database");
+  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+  return result?.value;
+};
+
 
 initdb();
